d8e46d01d6a17e82a02ef2a4db02100e3aa36087
Serhii Zhelobkov <32736906+melovarr@users.noreply.github.com>
2025-01-21 11:30:29 +0200

    chenged operators let to function

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,9 @@
-let getElementWidth(content, padding, border){
+function getElementWidth (content, padding, border) {
     return Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseInt(border) * 2 ;
 }
+
+
+
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
